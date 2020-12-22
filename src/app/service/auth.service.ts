74df8b307f5a5524b7a271c5e82fd0338d73d66b
GHagar <guerehounehajar@gmail.com>
2020-12-22 10:58:45 +0100

    third commit

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,18 +1,40 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  logout() {
-    throw new Error('Method not implemented.');
-  }
-  login(email: string, password: string) {
-    throw new Error('Method not implemented.');
-  }
-  signup(email: string, password: string) {
-    throw new Error('Method not implemented.');
+  user: Observable<any>;
+
+  constructor(private firebaseAuth: AngularFireAuth) {
+    this.user = firebaseAuth.authState;
   }
 
-  constructor() { }
+  signup(email: string, password: string) {
+    this.firebaseAuth
+      .createUserWithEmailAndPassword(email, password)
+      .then(value => {
+        console.log('Success!', value);
+      })
+      .catch(err => {
+        console.log('Something went wrong:',err.message);
+      });
+  }
+
+  login(email: string, password: string) {
+    this.firebaseAuth
+      .signInWithEmailAndPassword(email, password)
+      .then(value => {
+        console.log('Nice, it worked!');
+      })
+      .catch(err => {
+        console.log('Something went wrong:',err.message);
+      });
+  }
+
+  logout() {
+    this.firebaseAuth.signOut();
+  }
 }
